91d9a37e781ba8f21db7714bafb083f18180ed5b
Abhishek <deshmukhabhi02@gmail.com>
2025-06-29 18:28:26 +0200

    #5: UR5 Config WIP

--- source/UR5_direct/UR5_direct/tasks/direct/ur5_direct/ur5_direct_env.py
+++ source/UR5_direct/UR5_direct/tasks/direct/ur5_direct/ur5_direct_env.py
@@ -18,6 +18,7 @@ from isaaclab.sim.spawners.from_files import GroundPlaneCfg, spawn_ground_plane
 from isaacsim.utils.assets import ISAACLAB_NUCLEUS_DIR
 from isaaclab.utils.math import sample_uniform, axis_angle_from_quat
 
+from . import ur5_control as ur_ctrl
 from .ur5_direct_env_cfg import Ur5DirectEnvCfg, OBS_DIM_CFG, STATE_DIM_CFG
 ASSET_DIR = f"{ISAACLAB_NUCLEUS_DIR}/Factory"
 
@@ -27,42 +28,300 @@ class Ur5DirectEnv(DirectRLEnv):
 
     def __init__(self, cfg: Ur5DirectEnvCfg, render_mode: str | None = None, **kwargs):
         cfg.observation_space = sum([OBS_DIM_CFG[obs] for obs in cfg.obs_order])
+        cfg.state_space = sum([STATE_DIM_CFG[state] for state in cfg.state_order])
+        cfg.observation_space += cfg.action_space
+        cfg.state_space += cfg.action_space
+        self.cfg_task = cfg.task
+        
         super().__init__(cfg, render_mode, **kwargs)
 
+        self._set_body_inertias()
+        self._init_tensors()
+        self._set_default_dynamics_parameters()
+        self._compute_intermediate_values(dt=self.physics_dt)
+
+    def _set_body_inertias(self):
+        """Note: this is to account for the asset_options.armature parameter in IGE."""
+        inertias = self._robot.root_physx_view.get_inertias()
+        offset = torch.zeros_like(inertias)
+        offset[:, :, [0, 4, 8]] += 0.01
+        new_inertias = inertias + offset
+        self._robot.root_physx_view.set_inertias(new_inertias, torch.arange(self.num_envs))
+
+    def _set_default_dynamics_parameters(self):
+        """Set parameters defining dynamic interactions."""
+        self.default_gains = torch.tensor(self.cfg.ctrl.default_task_prop_gains, device=self.device).repeat(
+            (self.num_envs, 1)
+        )
+
+        self.pos_threshold = torch.tensor(self.cfg.ctrl.pos_action_threshold, device=self.device).repeat(
+            (self.num_envs, 1)
+        )
+        self.rot_threshold = torch.tensor(self.cfg.ctrl.rot_action_threshold, device=self.device).repeat(
+            (self.num_envs, 1)
+        )
+
+        # Set masses and frictions.
+        self._set_friction(self._held_asset, self.cfg_task.held_asset_cfg.friction)
+        self._set_friction(self._fixed_asset, self.cfg_task.fixed_asset_cfg.friction)
+        self._set_friction(self._robot, self.cfg_task.robot_cfg.friction)
+        
+
+    def _set_friction(self, asset, value):
+        """Update material properties for a given asset."""
+        materials = asset.root_physx_view.get_material_properties()
+        materials[..., 0] = value  # Static friction.
+        materials[..., 1] = value  # Dynamic friction.
+        env_ids = torch.arange(self.scene.num_envs, device="cpu")
+        asset.root_physx_view.set_material_properties(materials, env_ids)
+
+
+    def _init_tensors(self):
+        """Initialize tensors once."""
+        self.identity_quat = (
+            torch.tensor([1.0, 0.0, 0.0, 0.0], device=self.device).unsqueeze(0).repeat(self.num_envs, 1)
+        )
+
+        # Control targets.
+        self.ctrl_target_joint_pos = torch.zeros((self.num_envs, self._robot.num_joints), device=self.device)
+        self.ctrl_target_fingertip_midpoint_pos = torch.zeros((self.num_envs, 3), device=self.device)
+        self.ctrl_target_fingertip_midpoint_quat = torch.zeros((self.num_envs, 4), device=self.device)
+
+        # Fixed asset.
+        self.fixed_pos_action_frame = torch.zeros((self.num_envs, 3), device=self.device)
+        self.fixed_pos_obs_frame = torch.zeros((self.num_envs, 3), device=self.device)
+        self.init_fixed_pos_obs_noise = torch.zeros((self.num_envs, 3), device=self.device)
+
+        # Held asset
+        held_base_x_offset = 0.0
+        held_base_z_offset = 0.0
+        
+        
+        self.held_base_pos_local = torch.tensor([0.0, 0.0, 0.0], device=self.device).repeat((self.num_envs, 1))
+        self.held_base_pos_local[:, 0] = held_base_x_offset
+        self.held_base_pos_local[:, 2] = held_base_z_offset
+        self.held_base_quat_local = self.identity_quat.clone().detach()
+
+        self.held_base_pos = torch.zeros_like(self.held_base_pos_local)
+        self.held_base_quat = self.identity_quat.clone().detach()
+        
+        # Computer body indices.
+        self.left_finger_body_idx = self._robot.body_names.index("left_inner_finger")
+        self.right_finger_body_idx = self._robot.body_names.index("right_inner_finger")
+        # self.fingertip_body_idx = self._robot.body_names.index("panda_fingertip_centered")
+
+        # Tensors for finite-differencing.
+        self.last_update_timestamp = 0.0  # Note: This is for finite differencing body velocities.
+        self.prev_fingertip_pos = torch.zeros((self.num_envs, 3), device=self.device)
+        self.prev_fingertip_quat = self.identity_quat.clone()
+        self.prev_joint_pos = torch.zeros((self.num_envs, 7), device=self.device)
+        
+        # Keypoint tensors.
+        self.target_held_base_pos = torch.zeros((self.num_envs, 3), device=self.device)
+        self.target_held_base_quat = self.identity_quat.clone().detach()
+
+        offsets = self._get_keypoint_offsets(self.cfg_task.num_keypoints)
+        self.keypoint_offsets = offsets * self.cfg_task.keypoint_scale
+        self.keypoints_held = torch.zeros((self.num_envs, self.cfg_task.num_keypoints, 3), device=self.device)
+        self.keypoints_fixed = torch.zeros_like(self.keypoints_held, device=self.device)
+
+        # Used to compute target poses.
+        self.fixed_success_pos_local = torch.zeros((self.num_envs, 3), device=self.device)
+        self.fixed_success_pos_local[:, 2] = 0.0
+
+        self.ep_succeeded = torch.zeros((self.num_envs,), dtype=torch.long, device=self.device)
+        self.ep_success_times = torch.zeros((self.num_envs,), dtype=torch.long, device=self.device)
+
+
+    def _get_keypoint_offsets(self, num_keypoints):
+        """Get uniformly-spaced keypoints along a line of unit length, centered at 0."""
+        keypoint_offsets = torch.zeros((num_keypoints, 3), device=self.device)
+        keypoint_offsets[:, -1] = torch.linspace(0.0, 1.0, num_keypoints, device=self.device) - 0.5
+
+        return keypoint_offsets
 
     def _setup_scene(self):
+        """Initialise Simulation Scene"""
+        spawn_ground_plane(prim_path="/World/ground", cfg=GroundPlaneCfg(), 
+                           translation=(0.0, 0.0, -1.05))
+        # spawn a usd file of a table into the scene
+        cfg = sim_utils.UsdFileCfg(usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Mounts/SeattleLabTable/table_instanceable.usd")
+        cfg.func(
+            "/World/envs/env_.*/Table", cfg, translation=(0.55, 0.0, 0.0), 
+            orientation=(0.70711, 0.0, 0.0, 0.70711)
+        )
+
         self.robot = Articulation(self.cfg.robot_cfg)
-        # add ground plane
-        spawn_ground_plane(prim_path="/World/ground", cfg=GroundPlaneCfg())
-        # clone and replicate
-        self.scene.clone_environments(copy_from_source=False)
-        # we need to explicitly filter collisions for CPU simulation
+        self._fixed_asset = Articulation(self.cfg_task.fixed_asset)
+        self._held_asset = Articulation(self.cfg_task.held_asset)
+    
         if self.device == "cpu":
-            self.scene.filter_collisions(global_prim_paths=[])
-        # add articulation to scene
-        self.scene.articulations["robot"] = self.robot
+            # we need to explicitly filter collisions for CPU simulation
+            self.scene.filter_collisions()
+        
+        # Add Articulation to the Scene
+        self.scene.articulations["robot"] = self._robot
+        self.scene.articulations["fixed_asset"] = self._fixed_asset
+        self.scene.articulations["held_asset"] = self._held_asset
+
         # add lights
         light_cfg = sim_utils.DomeLightCfg(intensity=2000.0, color=(0.75, 0.75, 0.75))
         light_cfg.func("/World/Light", light_cfg)
 
-    def _pre_physics_step(self, actions: torch.Tensor) -> None:
-        self.actions = actions.clone()
+    def _compute_intermediate_values(self, dt):
+        """Get values computed from raw tensors. This includes adding noise."""
+        # TODO: A lot of these can probably only be set once?
+        self.fixed_pos = self._fixed_asset.data.root_pos_w - self.scene.env_origins
+        self.fixed_quat = self._fixed_asset.data.root_quat_w
 
+        self.held_pos = self._held_asset.data.root_pos_w - self.scene.env_origins
+        self.held_quat = self._held_asset.data.root_quat_w
+
+        self.fingertip_midpoint_pos = self._robot.data.body_pos_w[:, self.fingertip_body_idx] - self.scene.env_origins
+        self.fingertip_midpoint_quat = self._robot.data.body_quat_w[:, self.fingertip_body_idx]
+        self.fingertip_midpoint_linvel = self._robot.data.body_lin_vel_w[:, self.fingertip_body_idx]
+        self.fingertip_midpoint_angvel = self._robot.data.body_ang_vel_w[:, self.fingertip_body_idx]
+
+        jacobians = self._robot.root_physx_view.get_jacobians()
+
+        self.left_finger_jacobian = jacobians[:, self.left_finger_body_idx - 1, 0:6, 0:7]
+        self.right_finger_jacobian = jacobians[:, self.right_finger_body_idx - 1, 0:6, 0:7]
+        self.fingertip_midpoint_jacobian = (self.left_finger_jacobian + self.right_finger_jacobian) * 0.5
+        self.arm_mass_matrix = self._robot.root_physx_view.get_generalized_mass_matrices()[:, 0:7, 0:7]
+        self.joint_pos = self._robot.data.joint_pos.clone()
+        self.joint_vel = self._robot.data.joint_vel.clone()
+
+        # Finite-differencing results in more reliable velocity estimates.
+        self.ee_linvel_fd = (self.fingertip_midpoint_pos - self.prev_fingertip_pos) / dt
+        self.prev_fingertip_pos = self.fingertip_midpoint_pos.clone()
+
+        # Add state differences if velocity isn't being added.
+        rot_diff_quat = torch_utils.quat_mul(
+            self.fingertip_midpoint_quat, torch_utils.quat_conjugate(self.prev_fingertip_quat)
+        )
+        rot_diff_quat *= torch.sign(rot_diff_quat[:, 0]).unsqueeze(-1)
+        rot_diff_aa = axis_angle_from_quat(rot_diff_quat)
+        self.ee_angvel_fd = rot_diff_aa / dt
+        self.prev_fingertip_quat = self.fingertip_midpoint_quat.clone()
+
+        joint_diff = self.joint_pos[:, 0:7] - self.prev_joint_pos
+        self.joint_vel_fd = joint_diff / dt
+        self.prev_joint_pos = self.joint_pos[:, 0:7].clone()
+
+        # Keypoint tensors.
+        self.held_base_quat[:], self.held_base_pos[:] = torch_utils.tf_combine(
+            self.held_quat, self.held_pos, self.held_base_quat_local, self.held_base_pos_local
+        )
+        self.target_held_base_quat[:], self.target_held_base_pos[:] = torch_utils.tf_combine(
+            self.fixed_quat, self.fixed_pos, self.identity_quat, self.fixed_success_pos_local
+        )
+
+        # Compute pos of keypoints on held asset, and fixed asset in world frame
+        for idx, keypoint_offset in enumerate(self.keypoint_offsets):
+            self.keypoints_held[:, idx] = torch_utils.tf_combine(
+                self.held_base_quat, self.held_base_pos, self.identity_quat, keypoint_offset.repeat(self.num_envs, 1)
+            )[1]
+            self.keypoints_fixed[:, idx] = torch_utils.tf_combine(
+                self.target_held_base_quat,
+                self.target_held_base_pos,
+                self.identity_quat,
+                keypoint_offset.repeat(self.num_envs, 1),
+            )[1]
+
+        self.keypoint_dist = torch.norm(self.keypoints_held - self.keypoints_fixed, p=2, dim=-1).mean(-1)
+        self.last_update_timestamp = self._robot._data._sim_timestamp
+
+    def _get_observations(self):
+        """Get Actor/Critic inputs as asymmetric critic"""
+        noisy_fixed_pos = self.fixed_pos_obs_frame + self.init_fixed_pos_obs_noise
+
+        prev_actions = self.actions.clone()
+
+        obs_dict = {
+            "fingertip_pos": self.fingertip_midpoint_pos,
+            "fingertip_pos_rel_fixed": self.fingertip_midpoint_pos - noisy_fixed_pos,
+            "fingertip_quat": self.fingertip_midpoint_quat,
+            "ee_linvel": self.ee_linvel_fd,
+            "ee_angvel": self.ee_angvel_fd,
+            "prev_actions": prev_actions,
+        }
+
+        state_dict = {
+            "fingertip_pos": self.fingertip_midpoint_pos,
+            "fingertip_pos_rel_fixed": self.fingertip_midpoint_pos - self.fixed_pos_obs_frame,
+            "fingertip_quat": self.fingertip_midpoint_quat,
+            "ee_linvel": self.fingertip_midpoint_linvel,
+            "ee_angvel": self.fingertip_midpoint_angvel,
+            "joint_pos": self.joint_pos[:, 0:7],
+            "held_pos": self.held_pos,
+            "held_pos_rel_fixed": self.held_pos - self.fixed_pos_obs_frame,
+            "held_quat": self.held_quat,
+            "fixed_pos": self.fixed_pos,
+            "fixed_quat": self.fixed_quat,
+            "task_prop_gains": self.task_prop_gains,
+            "pos_threshold": self.pos_threshold,
+            "rot_threshold": self.rot_threshold,
+            "prev_actions": prev_actions,
+        }
+        obs_tensors = [obs_dict[obs_name] for obs_name in self.cfg.obs_order + ["prev_actions"]]
+        obs_tensors = torch.cat(obs_tensors, dim=-1)
+        state_tensors = [state_dict[state_name] for state_name in self.cfg.state_order + ["prev_actions"]]
+        state_tensors = torch.cat(state_tensors, dim=-1)
+        return {"policy": obs_tensors, "critic": state_tensors}
+        
+    def _reset_buffers(self, env_ids):
+        """Reset Buffers"""
+        self.ep_succeeded[env_ids] = 0
+        
+    def _pre_physics_step(self, action):
+        """Apply policy actions with smoothing."""
+        env_ids = self.reset_buf.nonzero(as_tuple=False).squeeze(-1)
+        if len(env_ids) > 0:
+            self._reset_buffers(env_ids)
+
+        self.actions = (
+            self.cfg.ctrl.ema_factor * action.clone().to(self.device) + (1 - self.cfg.ctrl.ema_factor) * self.actions
+        )
+
+    def close_gripper_in_place(self):
+        """Keep gripper in current position as gripper closes."""
+        actions = torch.zeros((self.num_envs, 6), device=self.device)
+        ctrl_target_gripper_dof_pos = 0.0
+
+        # Interpret actions as target pos displacements and set pos target
+        pos_actions = actions[:, 0:3] * self.pos_threshold
+        self.ctrl_target_fingertip_midpoint_pos = self.fingertip_midpoint_pos + pos_actions
+
+        # Interpret actions as target rot (axis-angle) displacements
+        rot_actions = actions[:, 3:6]
+
+        # Convert to quat and set rot target
+        angle = torch.norm(rot_actions, p=2, dim=-1)
+        axis = rot_actions / angle.unsqueeze(-1)
+
+        rot_actions_quat = torch_utils.quat_from_angle_axis(angle, axis)
+
+        rot_actions_quat = torch.where(
+            angle.unsqueeze(-1).repeat(1, 4) > 1.0e-6,
+            rot_actions_quat,
+            torch.tensor([1.0, 0.0, 0.0, 0.0], device=self.device).repeat(self.num_envs, 1),
+        )
+        self.ctrl_target_fingertip_midpoint_quat = torch_utils.quat_mul(rot_actions_quat, self.fingertip_midpoint_quat)
+
+        target_euler_xyz = torch.stack(torch_utils.get_euler_xyz(self.ctrl_target_fingertip_midpoint_quat), dim=1)
+        target_euler_xyz[:, 0] = 3.14159
+        target_euler_xyz[:, 1] = 0.0
+
+        self.ctrl_target_fingertip_midpoint_quat = torch_utils.quat_from_euler_xyz(
+            roll=target_euler_xyz[:, 0], pitch=target_euler_xyz[:, 1], yaw=target_euler_xyz[:, 2]
+        )
+
+        self.ctrl_target_gripper_dof_pos = ctrl_target_gripper_dof_pos
+        self.generate_ctrl_signals()
+            
+        
     def _apply_action(self) -> None:
         self.robot.set_joint_effort_target(self.actions * self.cfg.action_scale, joint_ids=self._cart_dof_idx)
-
-    def _get_observations(self) -> dict:
-        obs = torch.cat(
-            (
-                self.joint_pos[:, self._pole_dof_idx[0]].unsqueeze(dim=1),
-                self.joint_vel[:, self._pole_dof_idx[0]].unsqueeze(dim=1),
-                self.joint_pos[:, self._cart_dof_idx[0]].unsqueeze(dim=1),
-                self.joint_vel[:, self._cart_dof_idx[0]].unsqueeze(dim=1),
-            ),
-            dim=-1,
-        )
-        observations = {"policy": obs}
-        return observations
 
     def _get_rewards(self) -> torch.Tensor:
         total_reward = compute_rewards(
--- source/UR5_direct/UR5_direct/tasks/direct/ur5_direct/ur5_direct_env_cfg.py
+++ source/UR5_direct/UR5_direct/tasks/direct/ur5_direct/ur5_direct_env_cfg.py
@@ -7,6 +7,7 @@ from isaaclab_assets.robots.cartpole import CARTPOLE_CFG
 
 import isaaclab.sim as sim_utils
 from isaaclab.assets import ArticulationCfg
+from isaaclab.actuators.actuator_cfg import ImplicitActuatorCfg
 from isaaclab.envs import DirectRLEnvCfg
 from isaaclab.scene import InteractiveSceneCfg
 from isaaclab.sim import SimulationCfg, PhysxCfg    
@@ -40,6 +41,32 @@ STATE_DIM_CFG = {
     "rot_threshold": 3,
 }
 
+@configclass
+class ObsRandCfg:
+    fixed_asset_pos = [0.001, 0.001, 0.001]
+
+
+@configclass
+class CtrlCfg:
+    ema_factor = 0.2
+
+    pos_action_bounds = [0.05, 0.05, 0.05]
+    rot_action_bounds = [1.0, 1.0, 1.0]
+
+    pos_action_threshold = [0.02, 0.02, 0.02]
+    rot_action_threshold = [0.097, 0.097, 0.097]
+
+    reset_joints = [1.5178e-03, -1.9651e-01, -1.4364e-03, -1.9761, -2.7717e-04, 1.7796, 7.8556e-01]
+    reset_task_prop_gains = [300, 300, 300, 20, 20, 20]
+    reset_rot_deriv_scale = 10.0
+    default_task_prop_gains = [100, 100, 100, 30, 30, 30]
+
+    # Null space parameters.
+    default_dof_pos_tensor = [-1.3003, -0.4015, 1.1791, -2.1493, 0.4001, 1.9425, 0.4754]
+    kp_null = 10.0
+    kd_null = 6.3246
+    
+    
 @configclass
 class Ur5DirectEnvCfg(DirectRLEnvCfg):
     
@@ -129,30 +156,72 @@ class Ur5DirectEnvCfg(DirectRLEnvCfg):
         ),
         init_state=ArticulationCfg.InitialStateCfg(
             joint_pos={
-                "base_joint":0.00871,
-                "shoulder_pan_joint":,
-                "shoulder_lift_joint": ,
-                "elbow_joint":,
-                "wrist_1_joint":,
-                "wrist_2_joint":,
-                "wrist_3_joint":,
-                "left_outer_knuckle":,
-            }
-        )
+                "base_joint": 0.0,
+                "shoulder_pan_joint": -1.57,
+                "shoulder_lift_joint": -1.57,
+                "elbow_joint": 1.57,
+                "wrist_1_joint": -1.57,
+                "wrist_2_joint": 1.57,
+                "wrist_3_joint": 0.0,
+                "left_outer_knuckle": 0.0
+            },
+            pos=(0.0, 0.0, 0.0),
+            rot=(1.0, 0.0, 0.0, 0.0),
+        ),
+        actuators={
+            "ur_arm1": ImplicitActuatorCfg(
+                joint_names_expr=[
+                    "base_joint",
+                    "shoulder_pan_joint",
+                    "shoulder_lift_joint",
+                    "elbow_joint"
+                ],
+                stiffness=0.0,
+                damping=0.0,
+                friction=0.0,
+                armature=0.0,
+                effort_limit=150.0,
+                velocity_limit=2.0,
+            ),
+            "ur5_arm2": ImplicitActuatorCfg(
+                joint_names_expr=[
+                    "wrist_1_joint",
+                    "wrist_2_joint",
+                    "wrist_3_joint"
+                ],
+                stiffness=0.0,
+                damping=0.0,
+                friction=0.0,
+                armature=0.0,
+                effort_limit=75.0,
+                velocity_limit=3.0,
+            ),
+            "ur5_gripper": ImplicitActuatorCfg(
+                joint_names_expr=[
+                    "left_outer_knuckle"
+                ],
+                stiffness=7500.0,
+                damping=173.0,
+                friction=0.1,
+                armature=0.0,
+                effort_limit=40.0,
+                velocity_limit=0.04
+            )
+        }
     )
 
-    # custom parameters/scales
-    # - controllable joint
-    cart_dof_name = "slider_to_cart"
-    pole_dof_name = "cart_to_pole"
-    # - action scale
-    action_scale = 100.0  # [N]
-    # - reward scales
-    rew_scale_alive = 1.0
-    rew_scale_terminated = -2.0
-    rew_scale_pole_pos = -1.0
-    rew_scale_cart_vel = -0.01
-    rew_scale_pole_vel = -0.005
-    # - reset states/conditions
-    initial_pole_angle_range = [-0.25, 0.25]  # pole angle sample range on reset [rad]
-    max_cart_pos = 3.0  # reset if cart exceeds this position [m]
+    # # custom parameters/scales
+    # # - controllable joint
+    # cart_dof_name = "slider_to_cart"
+    # pole_dof_name = "cart_to_pole"
+    # # - action scale
+    # action_scale = 100.0  # [N]
+    # # - reward scales
+    # rew_scale_alive = 1.0
+    # rew_scale_terminated = -2.0
+    # rew_scale_pole_pos = -1.0
+    # rew_scale_cart_vel = -0.01
+    # rew_scale_pole_vel = -0.005
+    # # - reset states/conditions
+    # initial_pole_angle_range = [-0.25, 0.25]  # pole angle sample range on reset [rad]
+    # max_cart_pos = 3.0  # reset if cart exceeds this position [m]
